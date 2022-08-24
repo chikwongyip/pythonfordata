b7d8cbfeba8a5d804f97533eb71e1fd4668b6424
chikwongyip <527439024@qq.com>
2022-08-24 15:52:51 +0800

    pyplot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
-squares = [1,4,9,16,25]
-plt.plot(squares)
-plt.show
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(input_values, squares, linewidth=5)
+plt.title("square Numbers",fontsize="24")
+plt.xlabel("value",fontsize=14)
+plt.ylabel("Square of value", fontsize=14)
+
+plt.tick_params(axis="both", labelsize=14)
+plt.show()
